c4b031b89feaf14ed490c464ccbe549ec3f3e78f
koechkevin <koechkevin92@gmail.com>
2019-04-15 11:55:46 +0300

    .

--- src/api/Students/controller.js
+++ src/api/Students/controller.js
@@ -13,4 +13,38 @@ const createStudent = async (req, res) => {
     });
   }
 };
-export default { createStudent };
+
+const getStudents = async (req, res) => {
+  try {
+    const { query: { page } } = req;
+    const limit = req.query.limit || 10;
+    const count = await models.Student.count({
+      where: {
+        deletedAt: null
+      }
+    });
+    const pageCount = Math.ceil(count / limit);
+    const currentPage = page < 1 || !page || pageCount === 0 ? 1 : Math.min(page, pageCount);
+    const offset = limit * (currentPage - 1);
+    const students = await models.Student.findAll({
+      where: {
+        deletedAt: null,
+      },
+      offset,
+      limit,
+      order: [['id', 'DESC']]
+    });
+    res.status(200).json({
+      status: 'success',
+      students,
+      pagination: {
+        count, pageCount, currentPage
+      }
+    });
+  } catch (error) {
+    res.status(400).json({
+      error: error.message
+    });
+  }
+};
+export default { createStudent, getStudents };
--- src/api/Students/index.js
+++ src/api/Students/index.js
@@ -4,7 +4,7 @@ import controller from './controller';
 import validator from './validator';
 
 const { authenticate, allowRoles, } = auth;
-const { createStudent } = controller;
+const { createStudent, getStudents } = controller;
 const { checkIfStudentExists, validateStudent } = validator;
 
 const Router = express.Router();
@@ -15,6 +15,13 @@ Router.post(
   allowRoles(['Super Admin', 'Teacher', 'Manager']),
   validateStudent, checkIfStudentExists,
   createStudent
+);
+
+Router.get(
+  '/students',
+  authenticate,
+  allowRoles(['Super Admin', 'Teacher', 'Manager']),
+  getStudents
 );
 
 export default Router;
